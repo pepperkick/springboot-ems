328f8bdb404cec6d1dee500a5e54b0128342a5ae
pepperkick <abhishrant.m@gmail.com>
2019-09-25 14:22:20 +0530

    Added http status codes

--- src/main/java/com/pepperkick/ems/route/EmployeeRoute.java
+++ src/main/java/com/pepperkick/ems/route/EmployeeRoute.java
@@ -5,11 +5,15 @@ import com.pepperkick.ems.repository.EmployeeRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -24,7 +28,6 @@ public class EmployeeRoute {
     @RequestMapping(method= RequestMethod.GET, produces = "application/json")
     public List<Employee> get() {
         Iterable<Employee> employee = employeeRepository.findAll();
-        employee.forEach(obj -> logger.info("Employee: " + obj.getId() + " " + obj.getName() + " " + obj.getDesignation()));
         List<Employee> employees = new ArrayList<>();
 
         for (Employee e : employee) {
@@ -35,10 +38,14 @@ public class EmployeeRoute {
     }
 
     @RequestMapping(value= "/{id}", method= RequestMethod.GET)
-    public Employee get(@PathVariable int id) {
-        Optional<Employee> employee = employeeRepository.findById(id);
-        employee.ifPresent(obj -> logger.info("Employee: " + obj.getId() + " " + obj.getName() + " " + obj.getDesignation()));
+    public ResponseEntity get(@PathVariable int id, HttpServletResponse res) {
+        if (id < 0)
+            return new ResponseEntity(HttpStatus.NOT_ACCEPTABLE);
 
-        return employee.isPresent() ? employee.get() : null;
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isPresent())
+            return new ResponseEntity<Object>(employee, HttpStatus.OK);
+        else
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
 }
