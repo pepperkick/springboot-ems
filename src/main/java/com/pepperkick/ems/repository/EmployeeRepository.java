5137d7bac75a8a01848bfd5122c650ff316102b5
pepperkick <abhishrant.m@gmail.com>
2019-09-26 09:04:29 +0530

    Added new functions

--- src/main/java/com/pepperkick/ems/repository/EmployeeRepository.java
+++ src/main/java/com/pepperkick/ems/repository/EmployeeRepository.java
@@ -1,6 +1,8 @@
 package com.pepperkick.ems.repository;
 
+import com.pepperkick.ems.entity.Designation;
 import com.pepperkick.ems.entity.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
-
+    List<Employee> findEmployeeByDesignation(Designation designation);
 }
