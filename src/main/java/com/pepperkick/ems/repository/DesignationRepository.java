38895402dc04ff233c58312a06687a5b239128d0
pepperkick <abhishrant.m@gmail.com>
2019-09-26 09:03:03 +0530

    Added new function

--- src/main/java/com/pepperkick/ems/repository/DesignationRepository.java
+++ src/main/java/com/pepperkick/ems/repository/DesignationRepository.java
@@ -5,7 +5,10 @@ import com.pepperkick.ems.entity.Employee;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DesignationRepository extends CrudRepository<Designation, Integer> {
     Designation findByTitle(String title);
+    List<Designation> findByLevel(int level);
 }
