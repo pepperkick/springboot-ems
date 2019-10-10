05a1f841129f801478d54d9c2b653b4b34a62d90
pepperkick <abhishrant.m@gmail.com>
2019-10-10 16:02:52 +0530

    Removed default employee data

--- server/src/main/java/com/pepperkick/ems/service/EmployeeService.java
+++ server/src/main/java/com/pepperkick/ems/service/EmployeeService.java
@@ -20,23 +20,6 @@ public class EmployeeService {
     public EmployeeService(EmployeeRepository employeeRepository, DesignationRepository designationRepository, MessageHelper messageHelper) {
         this.employeeRepository = employeeRepository;
         this.messageHelper = messageHelper;
-
-        // Check if employee table is empty, if yes then fill with initial data
-        List<Employee> employees = employeeRepository.findAll();
-        if (employees.size() == 0) {
-            String[] names = { "Thor", "Iron Man", "Hulk", "Captain America", "War Machine", "Vision", "Falcon", "Ant Man", "Spider Man", "Black Widow" };
-            int[] designations = { 1, 2, 3, 2, 6, 5, 4, 3, 7, 4 };
-            int[] managers = { -1, 1, 1, 1, 2, 2, 4, 4, 2, 3};
-
-            for (int i = 0; i < 10; i++) {
-                Employee employee = new Employee();
-                employee.setName(names[i]);
-                employee.setDesignation(designationRepository.findById(designations[i]));
-                if (managers[i] != -1)
-                    employee.setManager(employeeRepository.findById(managers[i]));
-                employeeRepository.save(employee);
-            }
-        }
     }
 
     public Employee findById(int id) throws NotFoundException, BadRequestException {
